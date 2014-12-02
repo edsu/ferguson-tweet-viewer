14839e097b46c4106db7009745705d977fd64c2c
Ed Summers <ehs@pobox.com>
2014-12-02 20:43:23 +0000

    stacked view

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * This was quickly done, and it shows :)
  */
 
-var refresh = 2000;
+var refresh = 4000;
 var count = 0;
 var tweetWidth = 550;
 
@@ -27,18 +27,13 @@ function addDivs() {
   else {
     var w = 100;
     var middle = parseInt(height) / 2;
-    var vOffset1 = middle / 3;
-    var vOffset2 = middle / 1.5;
-    var verticalOffset = vOffset1;
     while (w < (width - tweetWidth)) {
       var div = $('<div class="tweet"></div>');
       div.css("position", "absolute");
       div.css("left", w);
-      div.css("top", middle - verticalOffset);
+      div.css("width", tweetWidth);
       $("body").append(div);
       w += tweetWidth;
-      verticalOffset = verticalOffset == vOffset1 ? vOffset2 : vOffset1;
-      console.log("added div");
     }
   }
 }
@@ -58,19 +53,16 @@ function addTweet() {
   var tweetId = getId();
   var divs = $(".tweet");
   var div = $(divs[count % divs.length]);
-  div.fadeOut('slow', function() {
-    div.empty();
-    twttr.widgets.createTweet(tweetId, div[0], {
-      align: 'center', 
-      theme: 'dark',
-      width: tweetWidth
-    }).then(function(p) {
-      setTimeout(function() {
-        $(p).parent().fadeIn('slow');
-      }, 500);
-      setTimeout(addTweet, refresh);
-    });
+  var tweet = $("<div></div>");
+  div.prepend(tweet);
+  twttr.widgets.createTweet(tweetId, tweet[0], {
+    align: 'center', 
+    theme: 'dark',
+    width: tweetWidth
+  }).then(function(t) {
+    $(t).style('margin', '30px');
   });
+  setTimeout(addTweet, refresh);
 }
 
 function getId() {
